6b019c700496cad536cdc252052a4341a0505962
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-12-23 16:23:14 -0800

    Add initial and cached install tests

--- purs-installer-bin/test/Test/PursInstaller/Spec.purs
+++ purs-installer-bin/test/Test/PursInstaller/Spec.purs
@@ -7,7 +7,7 @@ import Data.Identity (Identity)
 import Effect.Aff (Aff)
 import Node.Process (platformStr)
 import Test.PursInstaller.Setup (TestDirs, check, isSuccess)
-import Test.Spec (SpecT, describe, it)
+import Test.Spec (SpecT, it)
 import Test.Spec.Assertions.String (shouldContain)
 
 spec :: SpecT Aff TestDirs Identity Unit
@@ -19,23 +19,23 @@ spec = do
       | platformStr == "win32" = "-win"
       | otherwise = "-nix"
 
-  describe "can install PureScript" do
-    let
-      versions = 
-        [ "0.15.13"
-        , "0.15.0"
-        , "0.14.9"
-        , "0.14.0"
-        , "0.13.8"
-        , "0.13.0"
-        ]
-    for_ versions \version -> do
-      it (version <> ": Initial install") \{ pursInstaller, purs, checkFixture } -> do
-        let
-          checkStdout = checkFixture (version <> platformPrefix <> "-initial-stdout")
-        pursInstaller [ "install", "--purs-version", version ] >>= check { stdout: checkStdout, stderr: mempty, result: isSuccess }
-        purs [ "--version" ] >>= check { stdout: shouldContain version, stderr: mempty, result: isSuccess }
+  let
+    versions = 
+      [ "0.15.13"
+      , "0.15.0"
+      , "0.14.9"
+      , "0.14.0"
+      , "0.13.8"
+      , "0.13.0"
+      ]
+  for_ versions \version -> do
+    it (version <> ": Initial install") \{ pursInstaller, purs, checkFixture } -> do
+      let
+        checkStdout = checkFixture (version <> platformPrefix <> "-initial-stdout")
+      pursInstaller [ "install", "--purs-version", version ] >>= check { stdout: checkStdout, stderr: mempty, result: isSuccess }
+      purs [ "--version" ] >>= check { stdout: shouldContain version, stderr: mempty, result: isSuccess }
 
+  for_ versions \version -> do
       it (version <> ": Cached install") \{ pursInstaller, purs, checkFixture } -> do
         let
           checkStdout = checkFixture (version <> platformPrefix <> "-cached-stdout")
